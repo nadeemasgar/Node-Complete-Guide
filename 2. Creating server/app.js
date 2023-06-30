e37b82f436bb869568335420f0d2e5980c2e0ee8
Nadeem <nadeemasgar18@gmail.com>
2023-07-01 01:52:07 +0530

    Routing Request

--- 2. Creating server/app.js
+++ 2. Creating server/app.js
@@ -4,11 +4,18 @@ const http = require('http'); // require is used to import files in node.js
 // http.createServer(requestListner);
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit(); // to hard exist the event loop
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+      res.write('<html>');
+      res.write('<head><title>Enter Message</title><head>');
+      res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+      res.write('</html>');
+      return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
+    res.write('<head><title>My First Page</title><head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
