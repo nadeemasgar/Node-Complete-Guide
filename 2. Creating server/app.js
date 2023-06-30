38239f4345088fdde2578986cde556ebb34d18c9
Nadeem <nadeemasgar18@gmail.com>
2023-07-01 01:39:30 +0530

    Sending responses

--- 2. Creating server/app.js
+++ 2. Creating server/app.js
@@ -6,6 +6,12 @@ const http = require('http'); // require is used to import files in node.js
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
     // process.exit(); // to hard exist the event loop
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
