9fced9b57cde65de842ce9c89a1a4f03e1f7eba5
Nadeem <nadeemasgar18@gmail.com>
2023-07-02 00:38:55 +0530

    Event driven code execution

--- 2. Creating server/app.js
+++ 2. Creating server/app.js
@@ -19,16 +19,16 @@ const server = http.createServer((req, res) => {
         body.push(chunk);
     });
 
-    req.on('end', () => {
+    return req.on('end', () => {
         const parsedBody = Buffer.concat(body).toString();
         console.log(parsedBody);
         const message = parsedBody.split("=")[1];
-        fs.writeFileSync('message.txt', message);
+        fs.writeFile('message.txt', message, err => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     });
-    
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   res.setHeader('Content-Type', 'text/html');
