ef1b7c5cb3520caebf30662c35a09e28791fe494
Nadeem <nadeemasgar18@gmail.com>
2023-07-02 00:13:16 +0530

    parsing request bodies

--- 2. Creating server/app.js
+++ 2. Creating server/app.js
@@ -11,12 +11,26 @@ const server = http.createServer((req, res) => {
     res.write('</html>');
     return res.end();
   }
+
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
+    const body = [];
+    req.on('data', (chunk) => {
+        console.log(chunk);
+        body.push(chunk);
+    });
+
+    req.on('end', () => {
+        const parsedBody = Buffer.concat(body).toString();
+        console.log(parsedBody);
+        const message = parsedBody.split("=")[1];
+        fs.writeFileSync('message.txt', message);
+    });
+    
     res.statusCode = 302;
     res.setHeader('Location', '/');
     return res.end();
   }
+
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
   res.write('<head><title>My First Page</title><head>');
