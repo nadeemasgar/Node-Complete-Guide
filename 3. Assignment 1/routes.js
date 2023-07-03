b55b823a9fdcd6b2026fadd3cd888bcaf0b5095b
Nadeem <nadeemasgar18@gmail.com>
2023-07-04 00:10:51 +0530

    Solution of 3 & 4

--- 3. Assignment 1/routes.js
+++ 3. Assignment 1/routes.js
@@ -4,12 +4,31 @@ const requestHandler = (req, res) => {
 
     if(url == "/") {
         res.write("<html>");
-        res.write("<head><title>Assignment 1</title><head>");
-        res.write("<body><h1>Here is the Assignment</h1></body>");
+        res.write("<head><title>Enter Message</title><head>");
+        res.write(
+          '<body><form action="/username" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+        );
         res.write("</html>");
         return res.end();
     }
-    else if(url == "/users") {
+
+    if(url == '/username' && method == 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            res.statusCode = 302;
+            res.setHeader("Location", "/users");
+            return res.end();
+        })
+    }
+    
+    if(url == "/users") {
         res.write("<html>");
         res.write("<head><title>Assignment 1</title><head>");
         res.write("<body><ol><li>Nadeem</li><li>Rahul</li></ol></body>");
