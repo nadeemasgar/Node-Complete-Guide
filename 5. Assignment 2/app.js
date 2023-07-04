fb6f4c0b9ddc0a9af9b91ba81bc1b827f093cba3
Nadeem <nadeemasgar18@gmail.com>
2023-07-05 00:52:31 +0530

    more changes

--- 5. Assignment 2/app.js
+++ 5. Assignment 2/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
     res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
