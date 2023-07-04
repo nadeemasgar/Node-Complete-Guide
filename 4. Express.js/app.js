f6a63739556ee3d584459d4cff872515601d51f3
Nadeem <nadeemasgar18@gmail.com>
2023-07-05 00:02:47 +0530

    Handling different route

--- 4. Express.js/app.js
+++ 4. Express.js/app.js
@@ -2,15 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
+app.use('/', (req, res, next) => {
+    console.log('This always runs!');
     next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from Express!</h1>')
-})
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware of add product');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware of fallback route');
+    res.send('<h1>Hello from Express!</h1>');
+});
 
 app.listen(3000);
 
